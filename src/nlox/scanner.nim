a462769aee403a2254226830277d5a2cee6f25ff
rockcavera <rockcavera@gmail.com>
2023-08-22 21:30:54 -0300

    4.5 Recognizing Lexemes

--- src/nlox/scanner.nim
+++ src/nlox/scanner.nim
@@ -3,7 +3,7 @@ import ./token, ./tokentype
 type
   Scanner* = object
     source: string
-    tokens: seq[Token]
+    tokens: seq[Token] # list?
     start: int ## points to the first character in the lexeme
     current: int ## points at the character currently being considered
     line: int
@@ -17,13 +17,50 @@ proc initScanner*(source: string): Scanner =
 proc isAtEnd(scanner: Scanner): bool =
   result = scanner.current >= len(scanner.source)
 
-proc scanToken() =
-  discard
+proc advance(scanner: var Scanner): char =
+  result = scanner.source[scanner.current]
+
+  inc(scanner.current)
+
+proc addToken(scanner: var Scanner, kind: TokenType, literal: string) =
+  let text = scanner.source[scanner.start..scanner.current]
+
+  add(scanner.tokens, initToken(kind, text, literal, scanner.line))
+
+proc addToken(scanner: var Scanner, kind: TokenType) =
+  addToken(scanner, kind, "") # nil
+
+proc scanToken(scanner: var Scanner) =
+  let c = advance(scanner)
+
+  case c
+  of '(':
+    addToken(scanner, LeftParen)
+  of ')':
+    addToken(scanner, RightParen)
+  of '{':
+    addToken(scanner, LeftBrace)
+  of '}':
+    addToken(scanner, RightBrace)
+  of ',':
+    addToken(scanner, Comma)
+  of '.':
+    addToken(scanner, Dot)
+  of '-':
+    addToken(scanner, Minus)
+  of '+':
+    addToken(scanner, Plus)
+  of ';':
+    addToken(scanner, Semicolon)
+  of '*':
+    addToken(scanner, Star)
+  else:
+    discard
 
 proc scanTokens*(scanner: var Scanner): seq[Token] =
   while not isAtEnd(scanner):
     # We are at the beginning of the next lexeme.
     scanner.start = scanner.current
-    scanToken()
+    scanToken(scanner)
 
   add(scanner.tokens, initToken(EOF, "", "", scanner.line))
