4f1b0a5b69a09d88eefac72d0e9e221fedf4aeda
rockcavera <rockcavera@gmail.com>
2023-08-29 01:04:50 -0300

    adding comments to `src/nlox/astprinter.nim`

--- src/nlox/astprinter.nim
+++ src/nlox/astprinter.nim
@@ -1,25 +1,36 @@
+# Stdlib imports
 import std/strformat
 
+# Internal imports
 import ./expr, ./token, ./tokentype
 
+# Forward declaration
 proc parenthesize(name: string, exprs: varargs[Expr]): string
 
 method print(expr: Expr): string {.base.} =
+  ## Base method that raises `CatchableError` exception when `expr` has not had
+  ## its method implemented.
   raise newException(CatchableError, "Method without implementation override")
 
 method print(expr: Binary): string =
+  ## Returns a `string` representing the expression `expr`.
   parenthesize(expr.operator.lexeme, expr.left, expr.right)
 
 method print(expr: Grouping): string =
+  ## Returns a `string` representing the expression `expr`.
   parenthesize("group", expr.expression)
 
 method print(expr: Literal): string =
+  ## Returns a `string` representing the expression `expr`.
   $expr.value
 
 method print(expr: Unary): string =
+  ## Returns a `string` representing the expression `expr`.
   parenthesize(expr.operator.lexeme, expr.right)
 
 proc parenthesize(name: string, exprs: varargs[Expr]): string =
+  ## Returns a `string`, which encloses `name` and the subexpressions `exprs` in
+  ## parentheses. It is a helper procedure of `print()`.
   result = fmt"({name}"
 
   for expr in exprs:
@@ -27,6 +38,7 @@ proc parenthesize(name: string, exprs: varargs[Expr]): string =
 
   add(result, ')')
 
+# This may be removed in the future as it only serves to verify the above code.
 when isMainModule:
   proc main() =
     let expression = newBinary(
