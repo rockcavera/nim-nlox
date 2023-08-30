f3a9d373dc005c7cedcc5345786cfd46b8a1b6b7
rockcavera <rockcavera@gmail.com>
2023-08-30 16:47:02 -0300

    adding initialization procedures for `LiteralValue` objects

--- src/nlox/literals.nim
+++ src/nlox/literals.nim
@@ -18,6 +18,25 @@ type
     else:
       discard
 
+proc initLiteral*(): LiteralValue =
+  ## Initializes a `LiteralValue` object of kind `LitNull`
+  LiteralValue(kind: LitNull)
+
+proc initLiteralNumber*(numberLit: float): LiteralValue =
+  ## Initializes a `LiteralValue` object of kind `LitNumber` with the value
+  ## `numberLit`.
+  LiteralValue(kind: LitNumber, numberLit: numberLit)
+
+proc initLiteralString*(stringLit: string): LiteralValue =
+  ## Initializes a `LiteralValue` object of kind `LitString` with the value
+  ## `stringLit`.
+  LiteralValue(kind: LitString, stringLit: stringLit)
+
+proc initLiteralBoolean*(booleanLit: bool): LiteralValue =
+  ## Initializes a `LiteralValue` object of kind `LitBoolean` with the value
+  ## `booleanLit`.
+  LiteralValue(kind: LitBoolean, booleanLit: booleanLit)
+
 proc toString(lit: LiteralValue): string =
   ## Returns a string from the `LiteralValue` object.
   result = case lit.kind
