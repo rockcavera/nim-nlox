b9a6b08a9deaaec9b748822751098521f54d204d
rockcavera <rockcavera@gmail.com>
2023-08-30 15:45:14 -0300

    7.2.3 Evaluating unary expressions

--- src/nlox/interpreter.nim
+++ src/nlox/interpreter.nim
@@ -1,4 +1,4 @@
-import ./expr, ./token
+import ./expr, ./token, ./tokentype
 
 method evaluate(expr: Expr): LiteralValue {.base.} =
   raise newException(CatchableError, "Method without implementation override")
@@ -8,3 +8,14 @@ method evaluate(expr: Literal): LiteralValue =
 
 method evaluate(expr: Grouping): LiteralValue =
   evaluate(expr.expression)
+
+method evaluate(expr: Unary): LiteralValue =
+  let right = evaluate(expr.right)
+
+  case expr.operator.kind
+  of Bang:
+    result = LiteralValue(kind: LitBoolean, booleanLit: not isTruthy(right))
+  of Minus:
+    result = LiteralValue(kind: LitNumber, numberLit: -right.numberLit)
+  else:
+    result = LiteralValue(kind: LitNull) # You may need another type
