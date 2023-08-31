171bf41e3bd6c5b5b6d51835c54899b8190829da
rockcavera <rockcavera@gmail.com>
2023-08-31 16:43:14 -0300

    8.5.1 Nesting and shadowing

--- src/nlox/environment.nim
+++ src/nlox/environment.nim
@@ -3,25 +3,33 @@ import std/[tables, strformat]
 import ./literals, ./runtimeerror, ./token
 
 type
-  Environment* = object
+  Environment* = ref object
+    enclosing: Environment
     values: Table[string, LiteralValue]
 
-proc initEnvironment*(): Environment =
-  Environment(values: initTable[string, LiteralValue]())
+proc newEnvironment*(): Environment =
+  Environment(enclosing: nil, values: initTable[string, LiteralValue]())
 
-proc define*(environment: var Environment, name: string, value: LiteralValue) =
+proc newEnvironment*(enclosing: Environment): Environment =
+  Environment(enclosing: enclosing, values: initTable[string, LiteralValue]())
+
+proc define*(environment: Environment, name: string, value: LiteralValue) =
   environment.values[name] = value
 
-proc get*(environment: var Environment, name: Token): LiteralValue =
+proc get*(environment: Environment, name: Token): LiteralValue =
   if hasKey(environment.values, name.lexeme):
     result = environment.values[name.lexeme]
+  elif not isNil(environment.enclosing):
+    result = get(environment.enclosing, name)
   else:
     raise newRuntimeError(name, fmt"Undefined variable '{name.lexeme}'.")
 
-proc assign*(environment: var Environment, name: Token, value: LiteralValue) =
+proc assign*(environment: Environment, name: Token, value: LiteralValue) =
   if hasKey(environment.values, name.lexeme):
     environment.values[name.lexeme] = value
+  elif not isNil(environment.enclosing):
+    assign(environment.enclosing, name, value)
   else:
     raise newRuntimeError(name, fmt"Undefined variable '{name.lexeme}'.")
 
-var environment* = initEnvironment()
+var environment* = newEnvironment()
