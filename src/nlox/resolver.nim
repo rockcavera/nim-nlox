68d993aa52212614434861b65c41f6714750f068
rockcavera <rockcavera@gmail.com>
2023-09-06 21:09:39 -0300

    adding `Lox` state

--- src/nlox/resolver.nim
+++ src/nlox/resolver.nim
@@ -8,7 +8,7 @@ type
     scopes: seq[Table[string, bool]] # No stack collection in stdlib
 
 # Forward declaration
-proc resolve(statements: seq[Stmt])
+proc resolve*(lox: var Lox, statements: seq[Stmt])
 
 proc initResolver*(interpreter: Interpreter): Resolver =
   result.interpreter = interpreter
@@ -35,37 +35,35 @@ proc resolveLocal(resolver: var Resolver, expr: Expr, name: Token) =
       resolve(resolver.interpreter, hi - i)
       break
 
-method resolve(expr: Expr, resolver: var Resolver) {.base.} =
+method resolve(expr: Expr, resolver: var Resolver, lox: var Lox) {.base.} =
   raise newException(CatchableError, "Method without implementation override")
 
-method resolve(expr: Variable, resolver: var Resolver) =
+method resolve(expr: Variable, resolver: var Resolver, lox: var Lox) =
   if not(len(resolver.scopes) == 0) and (resolver.scopes[^1][expr.name.lexeme] == false):
     error(lox, expr.name, "Can't read local variable in its own initializer.")
 
   resolveLocal(resolver, expr, expr.name)
 
-method resolve(stmt: Stmt, resolver: var Resolver) {.base.} =
+method resolve(stmt: Stmt, resolver: var Resolver, lox: var Lox) {.base.} =
   raise newException(CatchableError, "Method without implementation override")
 
-method resolve(stmt: Block, resolver: var Resolver) =
+method resolve(stmt: Block, resolver: var Resolver, lox: var Lox) =
   beginScope(resolver)
 
-  resolve(stmt.statements)
+  resolve(lox, stmt.statements)
 
   endScope(resolver)
 
-method resolve(stmt: Var, resolver: var Resolver) =
+method resolve(stmt: Var, resolver: var Resolver, lox: var Lox) =
   declare(resolver, stmt.name)
 
   if not isNil(stmt.initializer):
-    resolve(stmt.initializer, resolver)
+    resolve(stmt.initializer, resolver, lox)
 
   define(resolver, stmt.name)
 
-proc resolve(statements: seq[Stmt]) =
-  var
-    interpreter: Interpreter
-    resolver = initResolver(interpreter)
+proc resolve*(lox: var Lox, statements: seq[Stmt]) =
+  var resolver = initResolver(lox.interpreter)
 
   for statement in statements:
-    resolve(statement, resolver)
+    resolve(statement, resolver, lox)
