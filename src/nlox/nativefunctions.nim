bcae5656e592c6ebd53a488de59b89519cc6615c
rockcavera <rockcavera@gmail.com>
2023-09-15 12:16:07 -0300

    added `-d:nloxBenchmark` switch to use `getMonoTime()` in clock()

--- src/nlox/nativefunctions.nim
+++ src/nlox/nativefunctions.nim
@@ -7,16 +7,28 @@ import ./environment, ./literals, ./types
 proc clockArity(caller: LoxCallable): int = 0
   ## `arity` for "clock()" .
 
-proc clockCall(caller: LoxCallable, interpreter: var Interpreter,
-               arguments: seq[Object]): Object =
-  ## `call` for "clock()".
-  let
-    currentTime = getTime()
-    seconds = float(toUnix(currentTime))
-    milliseconds = float(convert(Nanoseconds, Milliseconds,
-                                  nanosecond(currentTime))) / 1000.0
+when defined(nloxBenchmark):
+  import std/monotimes
 
-  result = newNumber(seconds + milliseconds)
+  proc clockCall(caller: LoxCallable, interpreter: var Interpreter,
+                 arguments: seq[Object]): Object =
+    ## `call` for "clock()".
+    let
+      current = ticks(getMonoTime())
+      milliseconds = float(convert(Nanoseconds, Milliseconds,current))
+
+    result = newNumber(milliseconds / 1000.0)
+else:
+  proc clockCall(caller: LoxCallable, interpreter: var Interpreter,
+                 arguments: seq[Object]): Object =
+    ## `call` for "clock()".
+    let
+      currentTime = getTime()
+      seconds = float(toUnix(currentTime))
+      milliseconds = float(convert(Nanoseconds, Milliseconds,
+                                   nanosecond(currentTime))) / 1000.0
+
+    result = newNumber(seconds + milliseconds)
 
 proc clockToString(caller: LoxCallable): string = "<native fn>"
   ## `toString` for "clock()".
